87dc085439bb007f7863b8781d9c8400c324d503
James <james@example.com>
2026-01-19 18:01:23 -0500

    - clean up

--- MCM.cpp
+++ MCM.cpp
@@ -1,119 +1,50 @@
-/*	MCM file compressor
-
-  Copyright (C) 2013, Google Inc.
-  Authors: Mathieu Chartier
-
-  LICENSE
-
-    This file is part of the MCM file compressor.
-
-    MCM is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    MCM is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with MCM.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#include <chrono>
-#include <ctime>
-#include <fstream>
-#include <windows.h>
-#include <eh.h>
-#include <deque>
-#include <vector>
-#include <array>
 #include <algorithm>
-#include <io.h>
-#include <fcntl.h>
+#include <array>
+#include <atomic>
 #include <cctype>
-#include "EntropySegmenter.h"
-#include "CCRConfig.h"
-#include "PathCover.h"
-#include "fingerprint.h"
-#include "Segmenter.h"
-#include "OracleChild.h"
-
-// --- Helper: Binary I/O ---
-#include "Util.hpp"
-template <typename T>
-void WriteBinary(const std::string& filename, const std::vector<T>& vec) {
-    FILE* f = fopen(filename.c_str(), "wb");
-    uint64_t size = vec.size();
-    fwrite(&size, sizeof(uint64_t), 1, f);
-    if (size > 0) fwrite(vec.data(), sizeof(T), size, f);
-    fclose(f);
-}
-
-template <typename T>
-std::vector<T> ReadBinary(const std::string& filename) {
-    FILE* f = fopen(filename.c_str(), "rb");
-    uint64_t size = 0;
-    fread(&size, sizeof(uint64_t), 1, f);
-    std::vector<T> vec(size);
-    if (size > 0) fread(vec.data(), sizeof(T), size, f);
-    fclose(f);
-    return vec;
-}
+#include <chrono>
+#include <cmath>
+#include <cerrno>
+#include <ctime>
+#include <deque>
+#include <fcntl.h>
+#include <fstream>
 #include <iomanip>
+#include <io.h>
 #include <limits>
 #include <map>
-#include <queue>
-#include <stdio.h>
-#include <string.h>
-#include <string>
-#include <sstream>
-#include <thread>
-#include <atomic>
 #include <mutex>
-#include <cmath>
 #include <numeric>
-
 #include <omp.h>
-
-#include <fstream>
+#include <queue>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <thread>
+#include <vector>
+#include <windows.h>
+#include <eh.h>
 
 #include "Archive.hpp"
+#include "CCRConfig.h"
 #include "CM.hpp"
 #include "CM-inl.hpp"
 #include "DeltaFilter.hpp"
 #include "Dict.hpp"
+#include "EntropySegmenter.h"
 #include "File.hpp"
+#include "fingerprint.h"
 #include "Huffman.hpp"
 #include "LZ-inl.hpp"
+#include "OracleChild.h"
+#include "PathCover.h"
 #include "ProgressMeter.hpp"
+#include "Segmenter.h"
 #include "Tests.hpp"
 #include "TurboCM.hpp"
+#include "Util.hpp"
 #include "X86Binary.hpp"
-
-class MemoryReadStream : public Stream {
-  const std::vector<uint8_t>& data;
-  size_t pos;
-public:
-  MemoryReadStream(const std::vector<uint8_t>& d) : data(d), pos(0) {}
-  int get() override {
-    if (pos >= data.size()) return EOF;
-    return data[pos++];
-  }
-  void put(int) override {} // no-op
-  size_t read(uint8_t* buf, size_t n) override {
-    size_t to_read = std::min(n, data.size() - pos);
-    if (to_read > 0) {
-      memcpy(buf, &data[pos], to_read);
-      pos += to_read;
-    }
-    return to_read;
-  }
-  void write(const uint8_t*, size_t) override {} // no-op
-  void seek(uint64_t p) override { pos = p; }
-  uint64_t tell() const override { return pos; }
-};
 
 #ifdef NDEBUG
 static constexpr bool kReleaseBuild = true;
@@ -169,8 +100,6 @@ float hellinger_distance(const MarkovMatrix& m1, const MarkovMatrix& m2) {
   }
   return sqrtf(dist);
 }
-
-
 
 static void printHeader() {
   std::cout << "MCM compressor" << std::endl;
@@ -390,7 +319,7 @@ public:
     if (mode == kModeUnknown) {
       const int remaining_args = argc - i;
       // No args, need to figure out what to do:
-      // decompress: mcm <archive> 
+      // decompress: mcm <archive>
       // TODO add files to archive: mcm <archive> <files>
       // create archive: mcm <files>
       mode = kModeCompress;
@@ -554,158 +483,7 @@ int main(int argc, char* argv[]) {
   case Options::kModeOpt:
   case Options::kModeCompress:
   case Options::kModeTest: {
-    if (options.files.empty()) {
-      options.usage(argv[0]);
-      return 1;
-    }
-    printHeader();
-
-    int err = 0;
-
-    std::string out_file = options.archive_file.getName();
-    File fout;
-
-    if (options.mode == Options::kModeOpt) {
-      std::cout << "Optimizing" << std::endl;
-      uint64_t best_size = std::numeric_limits<uint64_t>::max();
-      size_t best_var = 0;
-      std::ofstream opt_file("opt_result.txt");
-      // static const size_t kOpts = 10624;
-      static const size_t kOpts = 3;
-      // size_t opts[kOpts] = {0,1,2,3,15,14,4,6,7,8,9,17,12,11,13,5,10,18,20,19,21,26,22,28,23,24,16,25,27,29,31,32,36,33,34,35,37,30,38,39,};
-      // size_t opts[kOpts] = {}; for (size_t i = 0; i < kOpts; ++i) opts[i] = i;
-      size_t opts[kOpts] = {};
-      //size_t opts[] = {7,14,1,12,3,4,11,15,9,16,5,6,18,13,19,30,45,20,21,22,23,17,8,2,26,10,32,43,36,35,42,29,34,24,25,37,31,33,39,38,0,41,28,40,44,58,46,59,92,27,60,61,91,63,95,47,64,124,94,62,93,96,123,125,72,69,65,67,83,68,66,73,82,70,80,76,71,81,77,87,78,74,79,84,75,48,49,50,51,52,53,54,55,56,57,86,88,97,98,99,100,85,101,90,103,104,89,105,107,102,108,109,110,111,106,113,112,114,115,116,119,118,120,121,117,122,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,151,144,145,146,147,148,149,150,152,153,155,156,157,154,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,239,227,228,229,230,231,232,233,234,235,236,237,238,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,};
-      if (false) {
-        auto temp = ReadCSI<size_t>("optin.txt");
-        check(temp.size() == kOpts);
-        std::copy(temp.begin(), temp.end(), &opts[0]);
-      }
-      size_t best_opts[kOpts] = {};
-      srand(clock() + 291231);
-      size_t cur_index = 0;
-      size_t len = 1;
-      // size_t kMaxIndex = 128 - len;
-      // size_t kMaxIndex = cm::kModelCount;
-      size_t kMaxIndex = 12345;
-      size_t bads = 0;
-      size_t best_cur = 0;
-      static constexpr size_t kAvgCount = 2;
-      double total[kAvgCount] = {};
-      size_t count[kAvgCount] = {};
-      double min_time = std::numeric_limits<double>::max();
-      const bool kPerm = false;
-      if (kPerm) {
-        for (size_t i = 0;; ++i) {
-          if ((i & 255) == 255 && false) {
-            len -= len > 1;
-            kMaxIndex = 128 - len;
-          }
-          auto a = i % kMaxIndex;
-          auto b = (i + 1 + std::rand() % (kMaxIndex - 1)) % kMaxIndex;
-          VoidWriteStream fout;
-          Archive archive(&fout, options.options_);
-          if (i != 0) {
-            ReplaceSubstring(opts, a, len, b, kOpts);
-          }
-          if (!archive.setOpts(opts)) {
-            std::cerr << "Failed to set opts" << std::endl;
-            continue;
-          }
-          uint64_t in_bytes = archive.compress(options.files);
-          if (in_bytes == 0) continue;
-          const auto size = fout.tell();
-          std::cout << i << ": swap " << a << " to " << b << " " << size << std::endl;
-          if (size < best_size) {
-            std::cout << "IMPROVEMENT " << i << ": " << size << std::endl;
-            opt_file << i << ": " << size << " ";
-            for (auto opt : opts) opt_file << opt << ",";
-            opt_file << std::endl;
-            std::copy_n(opts, kOpts, best_opts);
-            best_size = size;
-          } else {
-            std::copy_n(best_opts, kOpts, opts);
-          }
-        }
-      } else {
-        for (auto o : opts) check(o <= kMaxIndex);
-        for (size_t i = 0;; ++i) {
-          const clock_t start = clock();
-          VoidWriteStream fout;
-          Archive archive(&fout, options.options_);
-          if (!archive.setOpts(opts)) {
-            continue;
-          }
-          uint64_t in_bytes = archive.compress(options.files);
-          if (in_bytes == 0) continue;
-          const double time = clockToSeconds(clock() - start);
-          total[i % kAvgCount] += time;
-          min_time = std::min(min_time, time);
-          const auto size = fout.tell();
-          opt_file << "opts ";
-          for (auto opt : opts) opt_file << opt << ",";
-          ++count[i % kAvgCount];
-          auto before_index = cur_index;
-          auto before_opt = opts[before_index];
-          if (size < best_size) {
-            best_size = size;
-            std::copy_n(opts, kOpts, best_opts);
-            best_var = opts[cur_index];
-            bads = 0;
-          } 
-          if (opts[cur_index] >= kMaxIndex) {
-            std::copy_n(best_opts, kOpts, opts);
-            cur_index = (cur_index + 1) % kOpts;
-            opts[cur_index] = 0;
-          } else {
-            ++opts[cur_index];
-          }
-
-          std::ostringstream ss;
-          double avgs[kAvgCount] = {};
-          for (size_t i = 0; i < kAvgCount; ++i) {
-            if (count[i] != 0) avgs[i] = total[i] / double(count[i]);
-          }
-          double avg = std::accumulate(total, total + kAvgCount, 0.0) / double(std::accumulate(count, count + kAvgCount, 0u));
-          ss << " -> " << formatNumber(size) << " best " << best_var << " in " << time << "s avg "
-             << avg << "(";
-          for (double d : avgs) ss << d << ",";
-          ss << ") min " << min_time;
-
-          opt_file << ss.str() << std::endl << std::flush;
-
-          std::cout << "opt[" << before_index << "]=" << before_opt << " best=" << best_var << "(" << formatNumber(best_size) << ") "
-            << formatNumber(in_bytes) << ss.str() << std::endl;
-        }
-      }
-    } else {
-      const clock_t start = clock();
-      if (err = fout.open(out_file, std::ios_base::out | std::ios_base::binary)) {
-        std::cerr << "Error opening: " << out_file << " (" << errstr(err) << ")" << std::endl;
-        return 2;
-      }
-
-      std::cout << "Compressing to " << out_file << " mode=" << options.options_.comp_level_ << " mem=" << options.options_.mem_usage_ << std::endl;
-      Archive archive(&fout, options.options_);
-      uint64_t in_bytes = archive.compress(options.files);
-      clock_t time = clock() - start;
-      std::cout << "Done compressing " << formatNumber(in_bytes) << " -> " << formatNumber(fout.tell())
-        << " in " << std::setprecision(3) << clockToSeconds(time) << "s"
-        << " bpc=" << double(fout.tell()) * 8.0 / double(in_bytes) << std::endl;
-
-      fout.close();
-
-      if (options.mode == Options::kModeSingleTest) {
-        if (err = fout.open(out_file, std::ios_base::in | std::ios_base::binary)) {
-          std::cerr << "Error opening: " << out_file << " (" << errstr(err) << ")" << std::endl;
-          return 1;
-        }
-        Archive archive(&fout);
-        archive.list();
-        std::cout << "Verifying archive decompression" << std::endl;
-        archive.decompress("", true);
-      }
-    }
+    // not used in our code
     break;
   }
   case Options::kModeAdd: {
@@ -775,15 +553,12 @@ int main(int argc, char* argv[]) {
   case Options::kModeObserver: {
     return runEntropyProfiler(options.files, options.archive_file);
   }
-
   case Options::kModeSegment: {
     if (options.files.empty()) { std::cerr << "Error: No input file." << std::endl; return 1; }
     std::string in_file = options.files[0].getName();
     runSegmenter(in_file, options);
     break;
   }
-
-  
   case Options::kModeFingerprint: {
     if (argc != 3) {
       std::cerr << "Usage: mcm -fingerprint <original_file>" << std::endl;
